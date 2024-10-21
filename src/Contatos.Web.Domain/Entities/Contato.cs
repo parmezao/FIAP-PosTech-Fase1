f4d55f209db24be679e8f8b31207c23f8161a374
parmezao <wagner@parmex.com.br>
2024-10-21 18:58:30 -0300

    Adicionadas validações de Email e Telefone

--- src/Contatos.Web.Domain/Entities/Contato.cs
+++ src/Contatos.Web.Domain/Entities/Contato.cs
@@ -1,18 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Contatos.Web.Infrastructure.CrossCutting.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Contatos.Web.Domain.Entities;
 
 public class Contato : BaseEntity
 {
     [Required(ErrorMessage ="Campo Nome é obrigatório!")]
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
 
     [Required(ErrorMessage = "Campo Email é obrigatório!")]
-    public string Email { get; set; }
+    [EmailAddress(ErrorMessage ="Email inválido!")]
+    public string? Email { get; set; }
 
     [Required(ErrorMessage = "Campo Telefone é obrigatório!")]
-    public string Telefone { get; set; }
+    [TelefoneValido(ErrorMessage ="Telefone inválido!")]
+    public string? Telefone { get; set; }
 
     [Required(ErrorMessage = "Campo DDD é obrigatório!")]
     public int DDD { get; set; }
 }
+
+
