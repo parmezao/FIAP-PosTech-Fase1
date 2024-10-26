8f447e877b82529ccd180138f7369225202e17e4
Wagner Cristiano Parmezão <wagner@parmex.com.br>
2024-10-26 09:54:53 -0300

    Refactoring nos testes da entidade Contato

--- src/Contatos.Web.Test/ContatoTest.cs
+++ src/Contatos.Web.Test/ContatoTest.cs
@@ -1,10 +1,12 @@
 using Contatos.Web.Domain.Entities;
 using Contatos.Web.Domain.ValueObjects;
+using System.Text.RegularExpressions;
 
 namespace Contatos.Web.Tests;
 
 public class ContatoTest
 {
+    #region Propriedade "Nome"
     [Fact(DisplayName = "Deve falhar quando nome for nulo")]
     public void Deve_falhar_quando_nome_for_nulo()
     {
@@ -67,5 +69,72 @@ public class ContatoTest
         // Assert
         Assert.Equal(valorEsperado, valorAtual);
     }
+    #endregion
 
+    #region Propriedade "Telefone"
+    [Fact]
+    public void Deve_falhar_se_telefone_tiver_mais_de_9_caracteres()
+    {        
+        const string TELEFONE_MAIOR_QUE_9_CARACTERES = "9888877660";
+
+        // Arrange
+        const int LIMITE_MAXIMO_CARACTERES = 9;
+
+        // Act
+        var contato = new Contato();
+        contato.Telefone = TELEFONE_MAIOR_QUE_9_CARACTERES;
+        int valorAtual = contato.Telefone.Length;
+
+        // Assert
+        Assert.True(valorAtual > LIMITE_MAXIMO_CARACTERES);
+    }
+
+    [Fact(DisplayName = "Deve falhar quando telefone for nulo")]
+    public void Deve_falhar_quando_telefone_for_nulo()
+    {
+        // Arrange        
+        string valorEsperado = null;
+
+        // Act
+        var contato = new Contato();
+        string valorAtual = contato.Telefone;
+
+        // Assert
+        Assert.Equal(valorEsperado, valorAtual);
+    }
+
+    [Fact(DisplayName = "Deve falhar quando telefone estiver em branco")]
+    public void Deve_falhar_quando_telefone_estiver_em_branco()
+    {
+        const string telefone_vazio = "";
+
+        // Arrange
+        string valorEsperado = telefone_vazio;
+
+        // Act
+        Contato contato = new() { Telefone = telefone_vazio };
+        string valorAtual = contato.Telefone;
+
+        // Assert
+        Assert.Equal(valorEsperado, valorAtual);
+    }
+
+    [Fact(DisplayName = "Deve falhar quando telefone possuir algum caractere alfanumerico")]
+    public void Deve_falhar_quando_telefone_possuir_algum_caractere_alfanumerico()
+    {
+        const string TELEFONE_COM_CARACTERE_ALFANUMERICO = "9888877X6";
+
+        var contato = new Contato();
+        contato.Telefone = TELEFONE_COM_CARACTERE_ALFANUMERICO;
+
+        // Arrange
+        bool valorEsperado = !Regex.IsMatch(contato.Telefone, "^[0-9]*$");
+
+        // Act
+
+        // Assert
+        Assert.True(valorEsperado);
+    }
+
+    #endregion
 }
